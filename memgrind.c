9d57f5de7b329f42fb26642f0916d713e3ff06c2
German A. Munguia <gam169@ice.cs.rutgers.edu>
2020-10-21 20:29:34 -0400

    timeofday

--- memgrind.c
+++ memgrind.c
@@ -1,98 +1,132 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
 #include "mymalloc.h"
 
 int main() {
 
 ///*
+	struct timeval timer;
 
-	//test case A
-		//malloc one byte and free it 120 times
-
-	char* ptr; 	
-	for(int i = 0; i < 120; i++) {
-		ptr = malloc(1*sizeof(char));
-		free(ptr);
-	}
-
-	//test case B 
-		//malloc 1 byte and store it in an array 120 times, then free them 1 by 1
-	
-	void* arr[120]; 
-	for(int i = 0; i < 120; i++) {
-		arr[i] = malloc(1);		
-	}
-
-	for(int i = 0; i < 120; i++) {
-		free(arr[i]);
-	}
+	gettimeofday(&timer, NULL); 
 
 
-	//test case C
-		//Randomly malloc or free 1 byte pointers 240 times
-	
-	//keep track of number of avaliable pointer
-	int pointerCount = 0; 
-	int mallocCount = 0;
-       	int freeCount = 0; 	
+	time_t startTime = timer.tv_sec;
+       	time_t startTime2 = timer.tv_usec; 	
+	printf("time: %ld\n", timer.tv_sec);
 
-	//keep track of the malloc pointers
-	void* arr2[120];
+	for(int workload = 0; workload < 1; workload++) {
 
-	for(int i = 0; i < 240; i++) {
-	
-		//if pointerCount is 0, then cannot free, must malloc and store in the array 	
-		if(pointerCount == 0) {
+		//test case A
+			//malloc one byte and free it 120 times
 
-			//printf("pointerCount: %d\tMust allocate\tmallocCount: %d\tfreeCount: %d\n", pointerCount, mallocCount, freeCount);
-
-			arr2[pointerCount] = malloc(1); 
-			pointerCount++; 
-			mallocCount++; 
-			continue; 
+		char* ptr; 	
+		for(int i = 0; i < 120; i++) {
+			ptr = malloc(1*sizeof(char));
+			free(ptr);
 		}
 
-		//if there are already 120 mallocs then it must free
-		if(mallocCount == 120) {
-
-			//printf("pointerCount: %d\t Max mallocs reach: %d\tfreeCount: %d\n", pointerCount, mallocCount, freeCount);
-
-			free(arr2[pointerCount-1]);
-			pointerCount--; 
-			freeCount++; 
-			continue; 
-		}
+		//test case B 
+			//malloc 1 byte and store it in an array 120 times, then free them 1 by 1
 		
-		//randomly generate a number between 0 and 1
-		int random = (rand() % 2);
+		void* arr[120]; 
+		for(int i = 0; i < 120; i++) {
+			arr[i] = malloc(1);		
+		}
 
-		//if 0 then malloc
-		if(random == 0) {
+		for(int i = 0; i < 120; i++) {
+			free(arr[i]);
+		}
+
+
+		//test case C
+			//Randomly malloc or free 1 byte pointers 240 times
+		
+		//keep track of number of avaliable pointer
+		int pointerCount = 0; 
+		int mallocCount = 0;
+		int freeCount = 0; 	
+
+		//keep track of the malloc pointers
+		void* arr2[120];
+
+		for(int i = 0; i < 240; i++) {
+		
+			//if pointerCount is 0, then cannot free, must malloc and store in the array 	
+			if(pointerCount == 0) {
+
+				//printf("pointerCount: %d\tMust allocate\tmallocCount: %d\tfreeCount: %d\n", pointerCount, mallocCount, freeCount);
+
+				arr2[pointerCount] = malloc(1); 
+				pointerCount++; 
+				mallocCount++; 
+				continue; 
+			}
+
+			//if there are already 120 mallocs then it must free
+			if(mallocCount == 120) {
+
+				//printf("pointerCount: %d\t Max mallocs reach: %d\tfreeCount: %d\n", pointerCount, mallocCount, freeCount);
+
+				free(arr2[pointerCount-1]);
+				pointerCount--; 
+				freeCount++; 
+				continue; 
+			}
 			
-			//printf("pointerCount: %d\tRandomly chose to malloc\tmallocCount: %d\tfreeCount: %d\n", pointerCount, mallocCount, freeCount);
+			//randomly generate a number between 0 and 1
+			int random = (rand() % 2);
 
-			arr2[pointerCount] = malloc(1); 
-			pointerCount++;
-			mallocCount++;
-			continue; 
+			//if 0 then malloc
+			if(random == 0) {
+				
+				//printf("pointerCount: %d\tRandomly chose to malloc\tmallocCount: %d\tfreeCount: %d\n", pointerCount, mallocCount, freeCount);
+
+				arr2[pointerCount] = malloc(1); 
+				pointerCount++;
+				mallocCount++;
+				continue; 
+			}
+
+			//if 1 then free
+			if(random == 1) {
+
+				//printf("pointerCount: %d\tRandomly chose to free  \tmallocCount: %d\tfreeCount: %d\n", pointerCount, mallocCount, freeCount);
+
+				free(arr2[pointerCount-1]);
+				pointerCount--;
+				freeCount++; 
+				continue;	
+			}
+
 		}
 
-		//if 1 then free
-		if(random == 1) {
 
-			//printf("pointerCount: %d\tRandomly chose to free  \tmallocCount: %d\tfreeCount: %d\n", pointerCount, mallocCount, freeCount);
 
-			free(arr2[pointerCount-1]);
-			pointerCount--;
-			freeCount++; 
-		       	continue;	
-		}
+		//test case D
+			//
+
 
 	}
 
-	//test case D
-		//
+	
+	gettimeofday(&timer, NULL);
+
+	time_t micro = (timer.tv_usec - startTime2); 
+	time_t seconds = (timer.tv_sec - startTime); 
+	
+	printf("seconds %ld\n", seconds);
+	printf("microseconds %ld\n", micro); 
+	double t = micro;
+      	t /= 1000000; 	
+	 printf("microseconds to seconds %f\n", t);
+
+
+//	gettimeofday(&timer,NULL); 
+//	printf("time: %ld\n", timer.tv_sec);
+	
+//	printf("\n TOOK %ld\n", (startTime-timer.tv_sec));
 
 
 /*
